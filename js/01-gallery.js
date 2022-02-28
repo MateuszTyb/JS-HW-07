6842166b083f1af2b88b4e19fdbd0644584ec2f4
Mateusz Tybinkowski <86377061+MateuszTyb@users.noreply.github.com>
2022-02-28 22:59:06 +0100

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+
+const galleryBox = document.querySelector("div.gallery");
+
+console.log(galleryItems);
+
+galleryItems.forEach((photo) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = photo.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.dataSource = photo.original;
+  galleryImage.alt = photo.description;
+  galleryImage.src = photo.preview;
+
+  galleryBox.append(galleryItem);
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+});
+
+galleryBox.addEventListener("click", (event) => {
+  event.preventDefault();
+  basicLightbox.create(`<img width="1280" src = ${event.target.dataSource}>`).show();
+});
+
+console.log(galleryItems);
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryBox = document.querySelector("ul.gallery");
+
+galleryItems.forEach((photo) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = photo.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.dataSource = photo.original;
+  galleryImage.alt = photo.description;
+  galleryImage.src = photo.preview;
+
+  galleryBox.append(galleryLink);
+  galleryLink.append(galleryImage);
+});
+
+galleryBox.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionSelector: "img",
+  captionPosition: "bottom",
+  captionType: "text",
+  captionsData: "alt",
+  sourceAttr: "href",
+});
+
+console.log(gallery);
